6f0a1b3a8413087b63aef99aa522d99a36215e19
João Sipaúba <joaosipauba@hotmail.com>
2020-08-25 11:01:01 -0300

    Inserção de scrollview na page addcategoria

--- src/screens/addCategoria.js
+++ src/screens/addCategoria.js
@@ -7,11 +7,11 @@ import {
   View,
   TextInput,
   Text,
-  Dimensions
+  Dimensions,
 } from "react-native";
 import { Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TouchableOpacity, ScrollView } from "react-native-gesture-handler";
 
 let ScreenHeight = Dimensions.get("window").height / 100;
 let ScreenWidth = Dimensions.get("window").width / 100;
@@ -20,7 +20,6 @@ function AddCategoria({ navigation }) {
   const [inputSection, setInputSection] = React.useState("");
   const [background, setBackground] = React.useState("#4CBE42");
   const [selecionado, setSelecionado] = React.useState(2);
-
 
   async function addSection() {
     if (inputSection === "Excluir") {
@@ -31,7 +30,11 @@ function AddCategoria({ navigation }) {
     } else {
       if (inputSection !== "") {
         let categorias = [];
-        let data = { categoria: inputSection.trim(), cor: background, lista: [] };
+        let data = {
+          categoria: inputSection.trim(),
+          cor: background,
+          lista: [],
+        };
 
         const myArray = await AsyncStorage.getItem("Categorias");
         if (myArray === null) {
@@ -67,62 +70,93 @@ function AddCategoria({ navigation }) {
       }
     }
   }
-  const cores = [ "#F45052", "#FEB400", "#4CBE42", "#F9931F", "#567DF4"];
+  const cores = ["#F45052", "#FEB400", "#4CBE42", "#F9931F", "#567DF4"];
 
   return (
-    <>
-      <View
-        style={{
-          paddingTop:"25%",
-          flex: 1,
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <Text style={{alignSelf:"flex-start",marginLeft:ScreenWidth*3,fontSize: 27, color: "black", padding: "5%", fontWeight: "bold"}}>Cor:</Text>
-        <View style={{flexDirection:"row", marginTop:0}}>
-          {cores.map((cor, index) => (
-            <TouchableOpacity key={index} activeOpacity={0.8} onPress={()=> {
-              setBackground(cor)
-              setSelecionado(index)}}>
-              <View
-                style={{
-                  backgroundColor: cor,
-                  height: ScreenHeight*8,
-                  width: ScreenWidth*16,
-                  borderRadius: 4,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  marginBottom: ScreenHeight*4,
-                  marginHorizontal:2
+      <ScrollView>
+        <View
+          style={{
+            paddingTop: "25%",
+            flex: 1,
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              alignSelf: "flex-start",
+              marginLeft: ScreenWidth * 3,
+              fontSize: 27,
+              color: "black",
+              padding: "5%",
+              fontWeight: "bold",
+            }}
+          >
+            Cor:
+          </Text>
+          <View style={{ flexDirection: "row", marginTop: 0 }}>
+            {cores.map((cor, index) => (
+              <TouchableOpacity
+                key={index}
+                activeOpacity={0.8}
+                onPress={() => {
+                  setBackground(cor);
+                  setSelecionado(index);
                 }}
               >
-                {selecionado === index?<Icon name="check" size={35} color="white" />:<Text></Text> }
-              </View>
-            </TouchableOpacity>
-          ))}
-        </View>
+                <View
+                  style={{
+                    backgroundColor: cor,
+                    height: ScreenHeight * 8,
+                    width: ScreenWidth * 16,
+                    borderRadius: 4,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginBottom: ScreenHeight * 4,
+                    marginHorizontal: 2,
+                  }}
+                >
+                  {selecionado === index ? (
+                    <Icon name="check" size={35} color="white" />
+                  ) : (
+                    <Text></Text>
+                  )}
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
 
-        <Text style={{alignSelf:"flex-start",marginLeft:ScreenWidth*3,fontSize: 27, color: "black", padding: "5%", fontWeight: "bold"}}>Nome da lista:</Text>
-        <View style={styles.viewInput}>
-          <TextInput
-            style={styles.input}
-            placeholder="Digite aqui"
-            placeholderTextColor="white"
-            onChangeText={(e) => setInputSection(e)}
-            value={inputSection}
-            maxLength={20}
-            autoFocus={true}
-          />
-          <Button
-            type="clear"
-            icon={<Icon name="plus-square" size={35} color="white" />}
-            style={{ marginVertical: 0 }}
-            onPress={addSection}
-          />
+          <Text
+            style={{
+              alignSelf: "flex-start",
+              marginLeft: ScreenWidth * 3,
+              fontSize: 27,
+              color: "black",
+              padding: "5%",
+              fontWeight: "bold",
+            }}
+          >
+            Nome da lista:
+          </Text>
+          <View style={styles.viewInput}>
+            <TextInput
+              style={styles.input}
+              placeholder="Digite aqui"
+              placeholderTextColor="white"
+              onChangeText={(e) => setInputSection(e)}
+              value={inputSection}
+              maxLength={20}
+              autoFocus={true}
+            />
+            <Button
+              type="clear"
+              icon={<Icon name="plus-square" size={35} color="white" />}
+              style={{ marginVertical: 0 }}
+              onPress={addSection}
+            />
+          </View>
         </View>
-      </View>
-    </>
+      </ScrollView>
   );
 }
 const styles = StyleSheet.create({
